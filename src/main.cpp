8831bf97f835d99513f63a93367d13f00002a9c7
piyooshsingh03 <piyoosh.ks2000@gmail.com>
2025-09-17 19:19:51 +0530

    Multi user working project

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#define led_pin 2
+// put function declarations here:
+void LEDState(int state)
+{
+  digitalWrite(led_pin,state);
+  Serial.println("Led State: "+String(state));
+};
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(led_pin,OUTPUT);
+}
+
+void loop() {
+  LEDState(LOW);
+  delay(500);
+  LEDState(HIGH);
+  delay(500);
+}
