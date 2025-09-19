462473a9a93b4d00b514b129b8d6fdc9c25ce49c
piyooshsingh03 <piyoosh.ks2000@gmail.com>
2025-09-19 11:06:00 +0530

    code changed for MQTT connection with esp32

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,112 @@
-//This is only for testing pepose and this reposatry shared with ashish
-#include <Arduino.h>
-#define led_pin 2
-// put function declarations here:
-void LEDState(int state)
-{
-  digitalWrite(led_pin,state);
-  Serial.println("Led State: "+String(state));
-};
+#include <WiFi.h>
+#include "WiFiClientSecure.h"
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+/************************* WiFi Access Point *********************************/
 
+#define WLAN_SSID "****ssid****"
+#define WLAN_PASS "****pass****"
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  8883  // Using port 8883 for MQTTS
+// Adafruit IO Account Configuration
+// (to obtain these values, visit https://io.adafruit.com and click on Active Key)
+#define AIO_USERNAME "aioname"
+#define AIO_KEY      "724b81xxxxxxxxxxxxx61da2163bxxxx"
+
+int value=0;
+/************ Global State (you don't need to change this!) ******************/
+
+WiFiClientSecure client;  // WiFiFlientSecure for SSL/TLS support
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+void MQTT_connect();
+// io.adafruit.com root CA
+const char* adafruitio_root_ca = \
+   "-----BEGIN CERTIFICATE-----\n" \
+   "MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
+   "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+   "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
+   "QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
+   "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
+   "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
+   "9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
+   "CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
+   "nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
+   "43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
+   "T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
+   "gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
+   "BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
+   "TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
+   "DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
+   "hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
+   "06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
+   "PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
+   "YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
+   "CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+   "-----END CERTIFICATE-----\n";
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'test' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Humidity");
+Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Temperature");
+
+/*************************** Sketch Code ************************************/
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(led_pin,OUTPUT);
+ Serial.begin(115200);
+ delay(10);
+ Serial.println(F("Adafruit IO MQTTS (SSL/TLS) Example"));
+
+ // Connect to WiFi access point.
+ Serial.println(); Serial.println();
+ Serial.print("Connecting to ");
+ Serial.println(WLAN_SSID);
+ delay(1000);
+ WiFi.begin(WLAN_SSID, WLAN_PASS);
+ delay(2000);
+
+ while (WiFi.status() != WL_CONNECTED) {
+   delay(500);
+   Serial.print(".");
+ }
+ Serial.println();
+ Serial.println("WiFi connected");
+ Serial.println("IP address: "); Serial.println(WiFi.localIP());
+ client.setCACert(adafruitio_root_ca);
 }
+
 void loop() {
-  LEDState(LOW);
-  delay(500);
-  LEDState(HIGH);
-  delay(500);
+ // Ensure the connection to the MQTT server is alive (this will make the first
+ // connection and automatically reconnect when disconnected).  See the MQTT_connect
+ // function definition further below.
+ MQTT_connect();
+
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+ int8_t ret;
+ // Stop if already connected.
+ if (mqtt.connected()) {
+   return;
+ }
+ Serial.print("Connecting to MQTT... ");
+ uint8_t retries = 3;
+ while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+      Serial.println(mqtt.connectErrorString(ret));
+      Serial.println("Retrying MQTT connection in 5 seconds...");
+      mqtt.disconnect();
+      delay(5000); // wait 5 seconds
+      retries--;
+      if (retries == 0) {
+        while (1); // basically die and wait for WDT to reset me
+      }
+ }
+ Serial.println("MQTT Connected!");
 }
